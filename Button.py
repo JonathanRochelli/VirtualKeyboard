38fa5769dc76353921857998fd40d6886c434427
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2021-08-30 11:05:15 +0200

    Add constant variable

--- Button.py
+++ Button.py
@@ -1,3 +1,11 @@
+
+import cv2
+
+TEXT_X = 25
+TEXT_Y = 50
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 class Button():
     def __init__(self, pos, text, size, color = (255, 255, 255), opacity=0.6):
         self.pos = pos
@@ -11,15 +19,15 @@ class Button():
         w, h = self.size
         overlay = img.copy()
         cv2.rectangle(overlay, self.pos, [x + w, y + h], self.color , cv2.FILLED)
-        cv2.putText(overlay, self.text, [x + 25, y + 50 ], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
+        cv2.putText(overlay, self.text, [x + TEXT_X, y + TEXT_Y ], cv2.FONT_HERSHEY_SIMPLEX, 1, BLACK, 3)
         cv2.addWeighted(overlay, self.opacity, img, 1 - self.opacity, 0, img)
     
     def focus(self):
-        self.color = (255, 255, 255)
+        self.color = WHITE
         self.opacity = 1
     
     def unfocus(self):
-        self.color = (255, 255, 255)
+        self.color = WHITE
         self.opacity = 0.6
 
     def setColor(self, color):
